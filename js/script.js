68126324a0043a3b78ebae8160b9eb8d3c273bd6
Ivanna_1109 <ivanna.kovaliv1215@gmail.com>
2022-02-21 18:59:27 +0300

    commit 1859

--- js/script.js
+++ js/script.js
@@ -59,4 +59,21 @@ let playList = [
     }
     document.body.appendChild(list);
 
+ // Task 2
+
+let modal = document.getElementById('myModal');
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
